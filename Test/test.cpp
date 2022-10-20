81db2faa4802b72dc2bd9bee2dc754187edb29e1
nicolehernandez <nicolehernandez@localhost>
2022-10-20 19:10:18 -0400

    Commit 1

---
+++ Test/test.cpp
@@ -0,0 +1,18 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Oct 20, 2022
+ *      Author: nicolehernandez
+ */
+
+#include "test.h"
+
+test::test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+test::~test() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Test/test.h
@@ -0,0 +1,17 @@
+/*
+ * test.h
+ *
+ *  Created on: Oct 20, 2022
+ *      Author: nicolehernandez
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class test {
+public:
+	test();
+	virtual ~test();
+};
+
+#endif /* TEST_H_ */
